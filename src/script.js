4f8c6e598e284b0a2d7a2df40d79d0734acc8063
Namix23 <98861500+Namix23@users.noreply.github.com>
2023-02-05 03:23:25 -0500

    Inject variables into strings

--- src/script.js
+++ src/script.js
@@ -10,3 +10,19 @@ let database = [
 //Select Elements
 let searchResults = document.querySelector(".search-results"),
   searchInput = document.querySelector(".search-input");
+
+function filter() {
+  for (let i = 0; i < database.length; i++) {
+    searchResults.innerHTML += (
+      <div class="box">
+        <div class="box-left">
+          <h2>${database[i].name}</h2>
+          <p>${database[i].desc}</p>
+        </div>
+        <div class="img-right">
+          <img src="${database[i].img}" />
+        </div>
+      </div>
+    );
+  }
+}
