59649fec304bf8007fe4e92626e7f103de677769
Namix23 <98861500+Namix23@users.noreply.github.com>
2023-02-09 23:49:32 -0500

    Create function, variables

--- src/script.js
+++ src/script.js
@@ -13,16 +13,16 @@ let searchResults = document.querySelector(".search-results"),
 
 function filter() {
   for (let i = 0; i < database.length; i++) {
-    searchResults.innerHTML += (
+    searchResults.innerHTML += `
       <div class="box">
         <div class="box-left">
-          <h2>${database[i].name}</h2>
-          <p>${database[i].desc}</p>
+        <h2>${database[i].name}</h2>
+        <p>${database[i].desc}</p>
         </div>
         <div class="img-right">
-          <img src="${database[i].img}" />
+        <img src="${database[i].img}">
         </div>
-      </div>
-    );
+        </div>
+         `;
   }
 }
